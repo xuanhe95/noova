d49df8aff49a71d0fe525765acb398f1021e893c
athrala <110925591+athrala@users.noreply.github.com>
2024-12-02 13:22:00 -0800

    non-ascii

--- noova/tools/src/main/java/org/noova/tools/Parser.java
+++ noova/tools/src/main/java/org/noova/tools/Parser.java
@@ -19,20 +19,12 @@ public class Parser {
     public static String processWord (String rawText){
         // [Process Helper] remove non-eng word + normalize space
 
-        if(rawText!=null || !rawText.isEmpty()) return rawText;
+        if(rawText==null && rawText.isEmpty()) return rawText;
 //        return rawText.replaceAll("[^a-zA-Z0-9\\s]", " ")
 //                .replaceAll("\\s+", " ").trim();
 
-//        rawText = org.jsoup.Jsoup.parse(rawText).text();
-//        rawText = java.text.Normalizer.normalize(rawText, java.text.Normalizer.Form.NFKC);
-
         // Remove unwanted characters and normalize space
         return rawText.replaceAll("[^\\p{ASCII}]", " ") // non ascii remove
-                .replaceAll("[^a-zA-Z0-9\\s]", " ")
-//                .replace("Â©", " ")  // Replace misinterpreted ©
-//                .replace("â€™", " ") // Replace misinterpreted apostrophe
-//                .replace("â€œ", " ") // Replace misinterpreted opening quote
-//                .replace("â€", " ")  // Remove non-ASCII printable chars
                 .replaceAll("\\s+", " ")             // Normalize spaces
                 .trim();
     }
